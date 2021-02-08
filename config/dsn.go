d96c3ab1ad45f3a674dd2c2890e18c8589d30d2c
Leo Takaoka <le0tk0k@gmail.com>
2021-02-08 22:01:36 +0900

    connect mysql

---
+++ config/dsn.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+// DSN はdb接続のためのdata source nameを返す
+func DSN() string {
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
+	) + "?charset=utf8"
+}
--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/Le0tk0k/blog-server/repository"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	_, err := repository.NewDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
