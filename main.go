cdb48819b58c64974755e16c27d659f4d2952cbc
Leo Takaoka <le0tk0k@gmail.com>
2021-02-08 23:37:12 +0900

    setup golang-migrate

--- main.go
+++ main.go
@@ -4,15 +4,29 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Le0tk0k/blog-server/config"
+
 	"github.com/Le0tk0k/blog-server/repository"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	_, err := repository.NewDB()
 	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.New("file://db/migrations", "mysql://"+config.DSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = m.Up(); err != nil {
 		log.Fatal(err)
 	}
 
